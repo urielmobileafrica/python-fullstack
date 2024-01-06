b53c3a2296002f94eb579e4cf3f7e294d928e12c
Sam Keiru <sam@urielmobileafrica.com>
2024-01-06 03:20:05 +0300

    Practice work

---
+++ fundamentals/introduction/numbers.py
@@ -0,0 +1,20 @@
+print(type(24))
+print(type(3.9))
+print(type(3j))
+
+#Testing COnversion
+int_to_float = float(35)
+float_to_int = int(44.2)
+int_to_complex = complex(35)
+print(int_to_float)
+print(float_to_int)
+print(int_to_complex)
+print(type(int_to_float))
+print(type(float_to_int))
+print(type(int_to_complex))
+
+
+import random
+print(random.randint(1,10))
+
+print(random.randbytes(10))
---
+++ fundamentals/introduction/python_syntax.py
@@ -0,0 +1,43 @@
+dog = ('Bruce', 'cocker spaniel', 19, False)
+print(dog[0])		# output: Bruce
+print(dog)
+#dog[1] = 'dachshund'	# ERROR: cannot be modified ('tuple' object does not support item assignment)
+
+
+empty_list = []
+ninjas = ['Rozen', 'KB', 'Oliver']
+print(ninjas[2]) 	# output: Oliver
+ninjas[0] = 'Francis'
+ninjas.append('Michael')
+print(ninjas)	# output: ['Francis', 'KB', 'Oliver', 'Michael']
+ninjas.pop()
+print(ninjas)	# output: ['Francis', 'KB', 'Oliver']
+ninjas.pop(1)
+print(ninjas)	# output: ['Francis', 'Oliver']
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2023
+}
+print(thisdict["year"])
+print(thisdict)
+print(thisdict[f'{1}'])
+
+
+#testing dictionaries
+
+empty_dict = {}
+new_person = {'name': 'John', 'age': 38, 'weight': 160.2, 'has_glasses': False}
+new_person['name'] = 'Jack'	# updates if the key exists, adds a key-value pair if it doesn't
+new_person['hobbies'] = ['climbing', 'coding']
+print(new_person)	
+# output: {'name': 'Jack', 'age': 38, 'weight': 160.2, 'has_glasses': False, 'hobbies': ['climbing', 'coding']}
+w = new_person.pop('weight')	# removes the specified key and returns the value
+print(w)		# output: 160.2
+print(new_person)	
+# output: {'name': 'Jack', 'age': 38, 'has_glasses': False, 'hobbies': ['climbing', 'coding']}        
+
+
